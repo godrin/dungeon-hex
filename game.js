a64703234de0ee431517254bd193b79247624691
David Kamphausen <david.kamphausen@web.de>
2013-05-16 21:37:17 +0200

    begin

--- game.js
+++ game.js
@@ -1,4 +1,23 @@
-$(function() {
+var Cell=Backbone.Model.extend({
+});
 
-alert("G");
+var Field=Backbone.Collection.extend({
+  model:Cell
+});
+
+
+
+
+$(function() {
+  var w,h;
+  w=h=32;
+  var cells=[];
+  for(var i=0;i<w*h;i++)
+    cells.push(new Cell());
+  var field=new Field(cells);
+
+
+
+
+//  alert("G");
 });
