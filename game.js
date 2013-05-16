dbfdd1cefafe5c53df179a04292e91a1f5f79c6b
undermink <undermink@devtal.de>
2013-05-16 23:38:08 +0200

    wall-tests

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ var CellView=Backbone.View.extend({
     //$(this.el).html(this.model.get("wall")?"W":"0");
     this.$el.addClass("tile");
     if (this.model.get("wall"))
-    this.$el.addClass("floor");
+    this.$el.addClass("floor"+Math.floor(Math.random()*6));
     this.$el.css({left:this.model.get("x")*54,
     top:((this.model.get("x")%2)+this.model.get("y")*2)*36});
     //console.log("DING!");
@@ -22,7 +22,7 @@ var CellView=Backbone.View.extend({
 
 var FieldView=Backbone.View.extend({
   initialize:function(){
-    console.log("init");
+    //console.log("init");
     this.cells=this.model.map(function(cellModel){
       return new CellView({
 	model:cellModel});
@@ -33,7 +33,7 @@ var FieldView=Backbone.View.extend({
     console.log("render");
     _.each(this.cells,function(cell){
       cell.render();
-      console.log("cell",cell,self.el);
+     // console.log("cell",cell,self.el);
       $(self.el).append(cell.el);
     });
     //$(this.el).html("fgfh");
@@ -46,7 +46,7 @@ $(function() {
   w=h=16;
   var cells=[];
   for(var i=0;i<w*h;i++)
-    cells.push(new Cell({x:i%w,y:Math.floor(i/w),wall:(true)}));
+    cells.push(new Cell({x:i%w,y:Math.floor(i/w),wall:(i==16+1)}));
   var field=new Field(cells);
   var fieldView=new FieldView({el:"#field",model:field});
   fieldView.render();
