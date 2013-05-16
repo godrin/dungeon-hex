d61a201aa995353545a8a1bec1648461b3693d5b
undermink <undermink@devtal.de>
2013-05-16 22:12:59 +0200

    snapshot

--- game.js
+++ game.js
@@ -5,19 +5,45 @@ var Field=Backbone.Collection.extend({
   model:Cell
 });
 
+var CellView=Backbone.View.extend({
+  tagName:"div",
+  initialize:function(){
+  },
+  render:function(){
+  $(this.el).html("blabla");
+  //console.log("DING!");
+  }
+});
 
+var FieldView=Backbone.View.extend({
+  initialize:function(){
+    console.log("init");
+    this.cells=this.model.map(function(cellModel){
+    return new CellView({
+    model:cellModel});
+    });
+  },
+  render:function(){
+    console.log("render");
+    _.each(this.cells,function(cell){
+    cell.render();
+    console.log("cell",cell);
+    $(this.el).append(cell.el);
+    });
+    //$(this.el).html("fgfh");
+  }
+});
 
 
 $(function() {
   var w,h;
-  w=h=32;
+  w=h=3;
   var cells=[];
   for(var i=0;i<w*h;i++)
-    cells.push(new Cell());
+    cells.push(new Cell({x:i%w,y:Math.floor(i/w)}));
   var field=new Field(cells);
-
-
-
-
-//  alert("G");
+  var fieldView=new FieldView({el:"#field",model:field});
+  fieldView.render();
+  
+  //  alert("G");
 });
