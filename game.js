d1b3cc0bd164894210c8d78e020509d35189bc79
David Kamphausen <david.kamphausen@web.de>
2013-05-16 22:20:59 +0200

    fixed

--- game.js
+++ game.js
@@ -10,8 +10,8 @@ var CellView=Backbone.View.extend({
   initialize:function(){
   },
   render:function(){
-  $(this.el).html("blabla");
-  //console.log("DING!");
+    $(this.el).html(this.model.get("wall")?"W":"0");
+    //console.log("DING!");
   }
 });
 
@@ -19,16 +19,17 @@ var FieldView=Backbone.View.extend({
   initialize:function(){
     console.log("init");
     this.cells=this.model.map(function(cellModel){
-    return new CellView({
-    model:cellModel});
+      return new CellView({
+	model:cellModel});
     });
   },
   render:function(){
+    var self=this;
     console.log("render");
     _.each(this.cells,function(cell){
-    cell.render();
-    console.log("cell",cell);
-    $(this.el).append(cell.el);
+      cell.render();
+      console.log("cell",cell,self.el);
+      $(self.el).append(cell.el);
     });
     //$(this.el).html("fgfh");
   }
@@ -40,10 +41,10 @@ $(function() {
   w=h=3;
   var cells=[];
   for(var i=0;i<w*h;i++)
-    cells.push(new Cell({x:i%w,y:Math.floor(i/w)}));
+    cells.push(new Cell({x:i%w,y:Math.floor(i/w),wall:(i%2==0)}));
   var field=new Field(cells);
   var fieldView=new FieldView({el:"#field",model:field});
   fieldView.render();
-  
+
   //  alert("G");
 });
