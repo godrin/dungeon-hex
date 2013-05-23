2c14544f8f744324fc5610023c7c9ebcc5fd4c8c
undermink <undermink@devtal.de>
2013-05-23 23:22:04 +0200

    make walls

--- game.js
+++ game.js
@@ -20,7 +20,7 @@ var Cell=Backbone.Model.extend({
   neighborCells:function(){
     var x=this.get("x");
     var y=this.get("y");
-    var neighbors=this.neighborDelta[x%2];
+    var neighbors=this.neighborDelta[(1+x)%2];
     return _.map(neighbors,function(neighbor){
       return {x:neighbor.x+x,y:neighbor.y+y};
     });
@@ -38,6 +38,7 @@ var Field=Backbone.Collection.extend({
     if(pos.x<0 || pos.y<0 || pos.x>=this.w || pos.y>=this.h)
       return null;
     var index=pos.x+pos.y*this.w;
+    console.log("IONDEX",index,this.at(index));
     return this.at(index);
   }
 });
@@ -48,13 +49,31 @@ var CellView=Backbone.View.extend({
   },
   render:function(){
 
-    var ns=this.model.neighbors(this.options.fieldModel);
-
     //$(this.el).html(this.model.get("wall")?"W":"0");
     this.$el.addClass("tile");
     this.$el.attr({x:this.model.get("x"),y:this.model.get("y")});
-    if (this.model.get("wall"))
+    if (!this.model.get("wall")) {
       this.$el.addClass("floor"+Math.floor(Math.random()*6));
+      var ns=this.model.neighbors(this.options.fieldModel);
+
+      // neighbor is wall
+      var nw=_.map(ns,function(n) {
+	return !n || (n.get("wall"));
+      });
+      console.log("NW",nw);
+      if(nw[0] && nw[5]) {
+	this.$el.append("<div class='swall tl_concave'></div>"); 
+      }
+      if(nw[4] && nw[5]) {
+	this.$el.append("<div class='swall l_concave'></div>"); 
+      }
+      if(nw[3] && nw[4]) {
+	this.$el.append("<div class='swall bl_concave'></div>"); 
+      }
+      if(nw[4] && !nw[3]) {
+	this.$el.append("<div class='swall r_convex'></div>"); 
+      }
+    }
     this.$el.css({left:this.model.get("x")*54,
       top:((this.model.get("x")%2)+this.model.get("y")*2)*36});
       //console.log("DING!");
@@ -91,7 +110,7 @@ $(function() {
   for(var i=0;i<w*h;i++)
   {
     var x=i%w,y=Math.floor(i/w);
-    cells.push(new Cell({x:x,y:y,wall:(x>3 && x<7 && y>3 && y<7)}));
+    cells.push(new Cell({x:x,y:y,wall:!(x>3 && x<7 && y>3 && y<7)}));
   }
 
   var field=new Field(cells);
